e9ee293ec0f32718bef7d5f7ccdce5c570498f5e
emberslee <854864430@qq.com>
2025-11-07 19:14:25 +0800

    add time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import cupy as cp
 
 from mrcio import parse_map, write_map
@@ -6,7 +7,11 @@ from mrcio import parse_map, write_map
 if __name__ == '__main__':
     cp.cuda.Device(0).use()
 
+    t0 = time.time()
     data, origin, _, voxel_size = parse_map(sys.argv[1], False, 1.0)
+    t1 = time.time()
+
+    print("Time = {:.4f}".format(t1 - t0))
 
     write_map(sys.argv[2], data, voxel_size, origin=origin)
 
